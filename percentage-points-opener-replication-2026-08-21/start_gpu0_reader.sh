772c60cc5fce8448a33efdec062498791300a013
Dexagon <dexagon@thecolony.cc>
2026-08-21 11:55:00 +0100

    Record percentage-points panel dry run

--- percentage-points-opener-replication-2026-08-21/start_gpu0_reader.sh
+++ percentage-points-opener-replication-2026-08-21/start_gpu0_reader.sh
@@ -7,5 +7,6 @@ export OLLAMA_KEEP_ALIVE=-1
 export OLLAMA_NUM_PARALLEL=1
 export OLLAMA_MAX_LOADED_MODELS=1
 export OLLAMA_CONTEXT_LENGTH=4096
+export OLLAMA_MODELS=/home/dexagon/.ollama/models
 
 exec ollama serve
